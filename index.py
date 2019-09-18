c6540e2a519a8b095f5c074dcf195e327589ff7c
Julien LM <julien.lemellec@gmail.com>
2019-09-18 22:02:22 +0200

    [css]: Add new template
    	- Add asset folder with css files
    	- Update index.py with well presented page

---
+++ index.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print"""
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Serre Bot Couarch</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="fade-in">
+
+				<!-- Intro -->
+					<div id="intro">
+						<h1>Serre<br />
+						Bot Couarch</h1>
+						<p>Automation system</p>
+						<ul class="actions">
+							<li><a href="#header" class="button icon solid solo fa-arrow-down scrolly">Continue</a></li>
+						</ul>
+					</div>
+
+				<!-- Header -->
+					<header id="header">
+						<a href="index.html" class="logo">Bot Couarch</a>
+					</header>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul class="links">
+							<li class="active"><a href="index.html">Capteurs</a></li>
+							<li><a href="generic.html">Actionneurs</a></li>
+							<li><a href="elements.html">Configuration</a></li>
+						</ul>
+						<ul class="icons">
+							<li><a href="https://www.facebook.com/PierreRabhiofficiel/" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="https://github.com/Julien-LM/green_house_monitoring" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+						</ul>
+					</nav>
+
+				<!-- Main -->
+					<div id="main">
+
+						<section class="post">
+
+						<!-- Table -->
+							<h3>Etat des capteurs de suveillances</h3>
+							<div class="table-wrapper">
+								<table>
+									<thead>
+										<tr>
+											<th>Capteur</th>
+											<th>Description</th>
+											<th>Etat</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td>Température</td>
+											<td>Température interne de la serre, à l'ombre.</td>
+											<td>XXXX</td>
+										</tr>
+										<tr>
+											<td>Humidité</td>
+											<td>Humidité ambiante de la serre en %.</td>
+											<td>XXXX</td>
+										</tr>
+										<tr>
+											<td>Luminosité</td>
+											<td>Luminosité de mesurée au centre de la serre (en lumen).</td>
+											<td>XXXX</td>
+										</tr>
+										<tr>
+											<td>Porte</td>
+											<td>Position de la porte (ouverte/fermée).</td>
+											<td>XXXX</td>
+										</tr>
+										<tr>
+											<td>Fenêtre</td>
+											<td>Position de la fenêtre (ouverte/fermée).</td>
+											<td>XXXX</td>
+										</tr>
+
+									</tbody>
+								</table>
+							</div>
+						</section>
+					</div>
+
+						
+
+				<!-- Footer -->
+					<footer id="footer">
+						<section>
+							<form method="post" action="#">
+								<div class="fields">
+									<div class="field">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name" />
+									</div>
+									<div class="field">
+										<label for="email">Email</label>
+										<input type="text" name="email" id="email" />
+									</div>
+									<div class="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="3"></textarea>
+									</div>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message" /></li>
+								</ul>
+							</form>
+						</section>
+						<section class="split contact">
+							<section class="alt">
+								<h3>Adresse</h3>
+								<p>Vallée verdoillante de Bot Couarch<br />
+								Paradise City, Breizh</p>
+							</section>
+							<section>
+								<h3>Phone</h3>
+								<p><a href="#">06.76.77.80.35</a></p>
+							</section>
+							<section>
+								<h3>Email</h3>
+								<p><a href="#">julien.lemellec@gmail.com</a></p>
+							</section>
+							<section>
+								<h3>Social</h3>
+								<ul class="icons alt">
+									<li><a href="https://www.facebook.com/PierreRabhiofficiel/" class="icon brands alt fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a href="https://github.com/Julien-LM/green_house_monitoring" class="icon brands alt fa-github"><span class="label">GitHub</span></a></li>
+								</ul>
+							</section>
+						</section>
+					</footer>
+
+				<!-- Copyright -->
+					<div id="copyright">
+						<ul><li>&copy; Bot Couarch Inc.</li><li>Design by: <a href="https://github.com/Julien-LM/green_house_monitoring">le T5</a></li></ul>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
+
+
+"""
